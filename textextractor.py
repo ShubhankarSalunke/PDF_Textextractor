fe4be46bbf74be63e1478185747ce9d7a0838467
ShubhankarSalunke <151939762+ShubhankarSalunke@users.noreply.github.com>
2025-01-27 16:29:48 +0530

    Initial commit

---
+++ textextractor.py
@@ -0,0 +1,15 @@
+from PyPDF2 import PdfReader
+
+
+path = #Path to the file
+
+reader = PdfReader(path)
+
+text = ""
+
+for page_num, page in enumerate(reader.pages, start =1):
+    print(f"Page Number : {page_num}")
+    print(page.extract_text())
+    print("-" * 100)
+
+print(text) 
